60f88529ec17f25e2a79a8389a8a4c9d03104ba0
seansabe <30910240+seansabe@users.noreply.github.com>
2017-10-05 23:18:34 -0500

    Add files via upload

--- vmlist.sh
+++ vmlist.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-VM='Windows-2008-64bit'
+VM='Ubuntu-Machine-One'
 VBoxManage createhd --filename $VM.vdi --size 32768
-VBoxManage createvm --name $VM --ostype "Windows2008_64" --register
+VBoxManage createvm --name $VM --ostype "Ubuntu_64" --register
 VBoxManage storagectl $VM --name "SATA Controller" --add sata --controller IntelAHCI
 VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VM.vdi
 VBoxManage storageattach $VM --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium /Users/seansabe/Desktop/taller/ubuntu.iso
@@ -13,9 +13,9 @@ VBoxManage modifyvm $VM --memory 1024 --vram 128
 VBoxManage modifyvm $VM --nic1 bridged --bridgeadapter1 e1000g0
 
 
-VM2='Windows-10-64bit'
+VM2='Ubuntu-Machine-Two'
 VBoxManage createhd --filename $VM2.vdi --size 32768
-VBoxManage createvm --name $VM2 --ostype "Windows10_64" --register
+VBoxManage createvm --name $VM2 --ostype "Ubuntu_64" --register
 VBoxManage storagectl $VM2 --name "SATA Controller" --add sata --controller IntelAHCI
 VBoxManage storageattach $VM2 --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VM2.vdi
 VBoxManage storageattach $VM2 --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium /Users/seansabe/Desktop/taller/ubuntu.iso
@@ -25,9 +25,9 @@ VBoxManage modifyvm $VM2 --memory 1024 --vram 128
 VBoxManage modifyvm $VM2 --nic1 bridged --bridgeadapter1 e1000g0
 
 
-VM4='MacOS-64bit'
+VM4='Ubuntu-Machine-Three'
 VBoxManage createhd --filename $VM4.vdi --size 32768
-VBoxManage createvm --name $VM4 --ostype "MacOS_64" --register
+VBoxManage createvm --name $VM4 --ostype "Ubuntu_64" --register
 VBoxManage storagectl $VM4 --name "SATA Controller" --add sata --controller IntelAHCI
 VBoxManage storageattach $VM4 --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VM4.vdi
 VBoxManage storageattach $VM4 --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium /Users/seansabe/Desktop/taller/ubuntu.iso
